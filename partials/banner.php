3d719794f8172d4118ed7cf29f26cd1861681935
Sleeplessmen <124755138+Sleeplessmen@users.noreply.github.com>
2023-11-30 23:49:35 +0700

    - change banner p1

--- partials/banner.php
+++ partials/banner.php
@@ -8,18 +8,18 @@
 
 						<a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
 							<div class="block1-txt-child1 flex-col-l">
-								<span class="block1-name ltext-102 trans-04 p-b-8">
-									Women
+								<span class="block1-name ltext-102 trans-04 p-b-8" style="font-family: 'Open Sans', sans-serif;">
+									Nhà sách online
 								</span>
 
-								<span class="block1-info stext-102 trans-04">
-									Spring 2018
+								<span class="block1-info stext-102 trans-04" style="font-family: 'Open Sans', sans-serif;">
+									Mùa đông 2023
 								</span>
 							</div>
 
 							<div class="block1-txt-child2 p-b-4 trans-05">
-								<div class="block1-link stext-101 cl0 trans-09">
-									Shop Now
+								<div class="block1-link stext-101 cl0 trans-09" style="font-family: 'Open Sans', sans-serif;">
+									Khám phá ngay
 								</div>
 							</div>
 						</a>
@@ -33,18 +33,18 @@
 
 						<a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
 							<div class="block1-txt-child1 flex-col-l">
-								<span class="block1-name ltext-102 trans-04 p-b-8">
-									Men
+								<span class="block1-name ltext-102 trans-04 p-b-8" style="font-family: 'Open Sans', sans-serif;">
+									Phương tiện di chuyển
 								</span>
 
-								<span class="block1-info stext-102 trans-04">
-									Spring 2018
+								<span class="block1-info stext-102 trans-04" style="font-family: 'Open Sans', sans-serif;">
+									Sản phẩm bán chạy nhất
 								</span>
 							</div>
 
 							<div class="block1-txt-child2 p-b-4 trans-05">
-								<div class="block1-link stext-101 cl0 trans-09">
-									Shop Now
+								<div class="block1-link stext-101 cl0 trans-09" style="font-family: 'Open Sans', sans-serif;">
+									Khám phá ngay
 								</div>
 							</div>
 						</a>
@@ -58,18 +58,18 @@
 
 						<a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
 							<div class="block1-txt-child1 flex-col-l">
-								<span class="block1-name ltext-102 trans-04 p-b-8">
-									Accessories
+								<span class="block1-name ltext-102 trans-04 p-b-8" style="font-family: 'Open Sans', sans-serif;">
+									Đồ chơi
 								</span>
 
-								<span class="block1-info stext-102 trans-04">
-									New Trend
+								<span class="block1-info stext-102 trans-04" style="font-family: 'Open Sans', sans-serif;">
+									Xu hướng mới
 								</span>
 							</div>
 
 							<div class="block1-txt-child2 p-b-4 trans-05">
-								<div class="block1-link stext-101 cl0 trans-09">
-									Shop Now
+								<div class="block1-link stext-101 cl0 trans-09" style="font-family: 'Open Sans', sans-serif;">
+									Khám phá ngay
 								</div>
 							</div>
 						</a>
