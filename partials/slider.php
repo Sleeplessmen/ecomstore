812f169a6c726462a19944712dc8a59fb072f3d8
cusnaruto <hoangvip3333@gmail.com>
2023-12-06 00:09:57 +0700

    viet hoa

--- partials/slider.php
+++ partials/slider.php
@@ -5,20 +5,20 @@
 					<div class="container h-full">
 						<div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
 							<div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
-								<span class="ltext-101 cl2 respon2">
-									Welcome to
+								<span class="ltext-101 cl2 respon2" style="font-family: 'Open Sans', sans-serif;">
+									Chào mừng đã đến với
 								</span>
 							</div>
 								
 							<div class="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay="800">
 								<h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-									ECOMSTORE
+									Shope
 								</h2>
 							</div>
 								
 							<div class="layer-slick1 animated visible-false" data-appear="zoomIn" data-delay="1600">
-								<a href="product.php?category=0" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-									Shop Now
+								<a href="product.php?category=0" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04" style="font-family: 'Open Sans', sans-serif;">
+									Mua sắm ngay
 								</a>
 							</div>
 						</div>
@@ -29,20 +29,20 @@
 					<div class="container h-full">
 						<div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
 							<div class="layer-slick1 animated visible-false" data-appear="rollIn" data-delay="0">
-								<span class="ltext-101 cl2 respon2">
-									Whatever you need
+								<span class="ltext-101 cl2 respon2" style="font-family: 'Open Sans', sans-serif;">
+									Bạn cần gì
 								</span>
 							</div>
 								
 							<div class="layer-slick1 animated visible-false" data-appear="lightSpeedIn" data-delay="800">
 								<h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-									It's here
+									Chúng tôi cũng có
 								</h2>
 							</div>
 								
 							<div class="layer-slick1 animated visible-false" data-appear="slideInUp" data-delay="1600">
-								<a href="product.php?category=9" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-									Shop Now
+								<a href="product.php?category=9" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04"  style="font-family: 'Open Sans', sans-serif;">
+									Mua sắm ngay
 								</a>
 							</div>
 						</div>
@@ -53,20 +53,20 @@
 					<div class="container h-full">
 						<div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
 							<div class="layer-slick1 animated visible-false" data-appear="rotateInDownLeft" data-delay="0">
-								<span class="ltext-101 cl2 respon2">
-									Men Collection 2023
+								<span class="ltext-101 cl2 respon2" style="font-family: 'Open Sans', sans-serif;">
+									Bộ sưu tập thời trang nam 2023
 								</span>
 							</div>
 								
 							<div class="layer-slick1 animated visible-false" data-appear="rotateInUpRight" data-delay="800">
 								<h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-									New arrivals
+									Đã có
 								</h2>
 							</div>
 								
 							<div class="layer-slick1 animated visible-false" data-appear="rotateIn" data-delay="1600">
-								<a href="product.php?category=5" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-									Shop Now
+								<a href="product.php?category=5" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04"  style="font-family: 'Open Sans', sans-serif;">
+									Mua sắm ngay
 								</a>
 							</div>
 						</div>
